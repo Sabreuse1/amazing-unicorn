bfa182922fce7d623cc7bd03794a206c3594338a
Natalie Cooper <natalie.cooper@nhm.ac.uk>
2020-01-15 12:55:35 +0000

    tidy script

--- script.R
+++ script.R
@@ -1,13 +1,3 @@
 # My messy script
 
 mydata <- read.scv("heelo.csv")
-
-<<<<<<< HEAD
-# This is amazing
-
-r <- my.code
-
-I HATE BISCUITS
-=======
-# This is amazing
->>>>>>> parent of 64f6dab... hdhdhshdadha
